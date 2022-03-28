917214311d91e41cb17bedbd9544c9feada08ae8
computador PRC <eduardo.reolon@paranacidade.org.br>
2022-03-28 10:55:50 -0300

    more comments

--- test/inicie.spec.ts
+++ test/inicie.spec.ts
@@ -3,6 +3,7 @@ import { string } from '@ioc:Adonis/Core/Helpers'
 import supertest from 'supertest'
 
 const BASE_URL = 'https://gorest.co.in/public/v2/'
+// O token ficaria nas variáveis de ambiente, deixei aqui para facilitar para o avaliador
 const token = 'b5e34cbc526e9e044144fe3f019d948ea27c324ad6905897a0b982d015027d75';
 const header = {
   Authorization: `Bearer ${token}`,
@@ -16,6 +17,8 @@ async function str_random(length = 5) {
     .toString('base64')
     .replace(/[^a-zA-Z0-9]/g, '')
 }
+
+// dados a serem inseridos na base
 const user:{id?,name,gender,email?,status} = {
   name: 'Tenali Ramakrishna',
   gender: 'male',
@@ -32,12 +35,18 @@ let firstPost:{id,user_id,title,body};
 const commentFirstPost:{id?,body} = {
   body: 'Any body content for the first post',
 }
-// caso não queira exibir todos os usuários do banco
-const maxPagesToList = 1;
+
+/**
+ * Para não visualizar todas as páginas, e agilizar o teste
+ * 0-exibe todas as páginas
+ */
+const maxPagesToList = 3;
 
 test.group('Inicie tests', async () => {
+  // cria o usuário na base
   await new Promise((resolve) => {
     test('Create user', async (assert) => {
+      // O domínio é uma string aleatória, para o caso de precisar rodar mais de 1 vez o mesmo teste
       user.email = `tenali.ramakrishna@${await str_random()}.com`;
       const response = await supertest(BASE_URL).post('users')
         .send(user)
@@ -51,6 +60,7 @@ test.group('Inicie tests', async () => {
     })
   })
 
+  // lista todos os usuários da base e exibe aquele recem criado
   await new Promise((resolve) => {
     test('List all users', async (assert) => {
       let currentPage = 0;
@@ -63,15 +73,18 @@ test.group('Inicie tests', async () => {
           .expect(200) as any;
         assert.exists(response._body);
         assert.isArray(response._body);
+        // exibe cada página
         console.log('Users ids', response._body.map((u) => u.id), 'Page', currentPage);
         usersInPage = response._body.length;
         if (!userServer) userServer = response._body.find((u) => u.id === user.id);
-      } while (usersInPage && currentPage < maxPagesToList);
+      } while (usersInPage && (!maxPagesToList || currentPage < maxPagesToList));
+      // exibe o usuário recem criado, que veio da busca
       console.log('User in server', userServer);
       resolve('Success!');
     }).timeout(0)
   })
 
+  // novo post para o usuário
   await new Promise((resolve) => {
     test('New post for the user', async (assert) => {
       const response = await supertest(BASE_URL).post('posts')
@@ -89,29 +102,32 @@ test.group('Inicie tests', async () => {
     })
   });
 
-  function newComment(post_id, comment) {
-    test('New comment', async (assert) => {
-      const response = await supertest(BASE_URL).post('comments')
-        .send({
-          ...comment,
-          post_id,
-          name: user.name,
-          email: user.email,
-        })
-        .set(header)
-        .expect(201) as any
-      assert.exists(response._body);
-      assert.exists(response._body.id);
-      comment.id = response._body.id;
-      console.log('Comment created', response._body);
-    })
+  // função para inserir comentário em um post. Para não repetir código
+  async function newComment(post_id, comment) {
+    await new Promise(async (resolve) => {
+      test('New comment', async (assert) => {
+        const response = await supertest(BASE_URL).post('comments')
+          .send({
+            ...comment,
+            post_id,
+            name: user.name,
+            email: user.email,
+          })
+          .set(header)
+          .expect(201) as any
+        assert.exists(response._body);
+        assert.exists(response._body.id);
+        comment.id = response._body.id;
+        console.log('Comment created', response._body);
+        resolve('Success!');
+      })
+    });
   }
   
-  await new Promise(async (resolve) => {
-    await newComment(postUser.id, commentUser);
-    resolve('Success!');
-  });
+  // criar comentário para o post do usuário
+  await newComment(postUser.id, commentUser);
 
+  // buscar primeira página dos posts da base
   await new Promise((resolve) => {
     test('Get all posts', async (assert) => {
       const response = await supertest(BASE_URL).get('posts')
@@ -125,11 +141,10 @@ test.group('Inicie tests', async () => {
     })
   });
 
-  await new Promise(async (resolve) => {
-    await newComment(firstPost.id, commentFirstPost);
-    resolve('Success!');
-  });
+  // criar comentário para o primeiro post da base
+  await newComment(firstPost.id, commentFirstPost);
 
+  // apagar o comentário recem criado
   await new Promise((resolve) => {
     test('Delete comment', async () => {
       await supertest(BASE_URL).delete(`comments/${commentFirstPost.id}`)
